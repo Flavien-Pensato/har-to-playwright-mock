a379bfe79a68bcb359e1148b7599ceab37de318f
Flavien PENSATO <flavien.pensato.ext@homeserve.fr>
2024-01-11 14:46:21 +0100

    docs(): add readme

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # har-to-playwright-mock
+
+A brief summary of what your Node.js CLI script does and its purpose.
+
+## Installation
+
+Make sure you have [Node.js](https://nodejs.org/) installed on your machine.
+
+Use npm to install the script globally.
+
+```bash
+npm install -g har-to-playwright-mock
+```
+
+## Usage
+
+A detailed explanation of how to use your CLI script. Include examples and screenshots if possible.
+
+```bash
+har-to-playwright-mock <path_har_file>
+```
+
+### Examples
+
+
+### Contributing
+
+You are welcome to pull request.
--- index.js
+++ index.js
@@ -2,29 +2,26 @@
 const fs = require('fs');
 
 async function createMockingScript(harFilePath) {
-    // Lire le fichier HAR
-    const harData = fs.readFileSync(harFilePath, 'utf-8');
-    const harJson = JSON.parse(harData);
+  const harData = fs.readFileSync(harFilePath, 'utf-8');
+  const harJson = JSON.parse(harData);
 
-    // Générer le script Playwright pour mocker chaque URL
-    const playwrightScript = `
-  // Créer des mocks pour chaque route dans le fichier HAR
-  let mocks = [
+  const playwrightScript = `
+    const mocks = [
     ${harJson.log.entries.map(entry => {
-        const url = new URL(entry.request.url)
-        const pathname = entry.request.url.split(url.host)[1]
-        return `{
-            url: "${pathname}",
-            response: {
-              status: ${entry.response.status},
-              body: ${JSON.stringify(entry.response.content.text)},
-              headers: ${JSON.stringify(entry.response.headers)}
-            },
-          }`
-    }).join(',')}
+    const url = new URL(entry.request.url)
+    const pathname = entry.request.url.split(url.host)[1]
+
+    return `{
+              url: "${pathname}",
+              response: {
+                status: ${entry.response.status},
+                body: ${JSON.stringify(entry.response.content.text)},
+                headers: ${JSON.stringify(entry.response.headers)}
+              },
+            }`
+  }).join(',')}
   ];
 
-  // Créer et activer les mocks dans le contexte de la page
   await page.route('**/*', async (route) => {
     const matchingMockIndex = mocks.findIndex(mock =>
       route.request().url().includes(mock.url)
@@ -39,7 +36,6 @@ async function createMockingScript(harFilePath) {
         headers: matchingMock.response.headers,
       });
 
-      // Supprimer le mock utilisé de la liste
       mocks.splice(matchingMockIndex, 1);
     } else {
       route.continue();
@@ -47,23 +43,21 @@ async function createMockingScript(harFilePath) {
   });
 `;
 
-    console.log(`Le script Playwright a été généré avec succès:\n\n${playwrightScript}`);
+  console.log(`The Playwright script have been generated with success :\n\n${playwrightScript}`);
 }
 
-// Si le script est exécuté directement depuis la ligne de commande
 if (require.main === module) {
-    const harFilePath = process.argv[2];
+  const harFilePath = process.argv[2];
 
-    if (!harFilePath) {
-        console.error('har-to-playwright-mock <chemin_fichier_har>');
-        process.exit(1);
-    }
-
-    createMockingScript(harFilePath);
-} else {
+  if (!harFilePath) {
     console.error('har-to-playwright-mock <chemin_fichier_har>');
     process.exit(1);
+  }
+
+  createMockingScript(harFilePath);
+} else {
+  console.error('har-to-playwright-mock <chemin_fichier_har>');
+  process.exit(1);
 }
 
-// Exporter la fonction pour pouvoir l'utiliser dans un autre script si nécessaire
 module.exports = createMockingScript;
